e531dbb991ed44fd7d87d3650dfa24b6b7b4f1e2
unknown <jhc0001@alu.medac.es>
2023-04-17 13:24:33 +0200

    TestAll

---
+++ com.entornos.JaimeHermana.array/com.entornos.JaimeHermana.array/src/com/entornos/JaimeHermana/array/test/AllTests.java
@@ -0,0 +1,10 @@
+package com.entornos.JaimeHermana.array.test;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ Tests.class })
+public class AllTests {
+
+}
